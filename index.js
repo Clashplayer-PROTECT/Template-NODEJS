4bce768ce577f591e660a868b3b32571e8569093
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2021-08-29 11:58:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('\n_________  __      __  _________\n\\_   ___ \\\/  \\    \/  \\\/   _____\/\n\/    \\  \\\/\\   \\\/\\\/   \/\\_____  \\ \n\\     \\____\\        \/ \/        \\\n \\______  \/ \\__\/\\  \/ \/_______  \/\n        \\\/       \\\/          \\\/ \n');
+console.log('Bienvenue sur votre serveur d\'hébergement NodeJS');
+console.log('>> Pour installer votre bot, connectez vous au serveur en SFTP');
+console.log('                                            ');                                                                                                              
+
+process.stdin.resume();
